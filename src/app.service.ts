a5cbde2f79b81f85544a51b90faefa0a49fe03e9
GuticaStefan <stefan.gutica@gmail.com>
2025-12-23 14:00:44 +0200

    add api key

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const apiKey = '550e8400-e29b-41d4-a716-446655440000';
     return 'Hello World!';
   }
 }
